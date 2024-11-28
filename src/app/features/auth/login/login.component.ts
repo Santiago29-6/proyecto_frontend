08c63716640438c606b9de7619f3f77306adcbbd
Santiago29-6 <chavarrokevin29@gmail.com>
2024-11-27 21:17:07 -0500

    Conexión de del backend y fron para envio de token
    - Se hace la conexión del backend y se hace el login
    - Cuando se hace el login le llega correctamente el token

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { AuthRequest } from '../../../shared/models/authRequest.model';
+import { AuthResponse } from '../../../shared/models/authResponse.model';
+import { AuthenticationService } from '../../../core/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm !: FormGroup;
+  authReq !: AuthRequest;
+  authRes !: AuthResponse;
+
+  constructor(
+    public fb: FormBuilder,
+    public authService: AuthenticationService
+  ){ }
+
+  ngOnInit():void{
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required , Validators.minLength(6)]]
+    });
+  }
+
+  login():void{
+    if(this.loginForm.valid){
+      this.authService.signIn(this.loginForm.value)
+      .subscribe({
+        next: (resp) => {
+          this.authRes = resp;
+          localStorage.setItem('authToken', this.authRes.token);
+          console.log(this.authRes);
+        },
+        error: (error) => {
+          console.error("No se puedo hacer el logueo por: ", error);
+        }
+      });
+    } else {
+      console.error('Formulario no válido:', this.loginForm.errors);
+    }
+  }
 
 }
---
+++ src/app/shared/components/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../../../core/services/authentication/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+
+}
