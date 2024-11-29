c7c64eae735268117c563f828c84d80d601555e3
Santiago29-6 <chavarrokevin29@gmail.com>
2024-11-29 17:35:19 -0500

    Se elimina componente y se agrega nuevo
    - Se agrega un nuevo componente
    - Se elimina un componente que no se esta utilizando
    - Se edita componente como lo son header y footer

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,25 @@
+import { Router } from '@angular/router';
+import { AuthenticationService } from './../../../core/services/authentication/authentication.service';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(
+    public authService: AuthenticationService,
+    public router: Router
+  ) { }
 
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']); // Redirige al login
+  }
 }
