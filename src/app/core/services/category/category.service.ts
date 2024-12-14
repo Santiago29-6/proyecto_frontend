4bd35a3074cc36697194396d145132eeb306ee66
Santiago29-6 <chavarrokevin29@gmail.com>
2024-12-13 20:33:54 -0500

    Se realizan todos los service
    - Se hacen los services que estaban pendientes por realizar
    - Se realizan cambios en otros services que habian

--- src/app/core/services/category/category.service.ts
+++ src/app/core/services/category/category.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../../shared/models/category.model';
+import { environment } from '../../../../envirorements/enviroments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(
+    private readonly httpClient : HttpClient,
+    private readonly authService: AuthService
+  ) { }
+
+  public getAllCategory() : Observable<Category[]> {
+    const headers = this.authService.createAuthHeaders();
+    return this.httpClient.get<Category[]>(environment.urlHost + "category", {headers: headers});
+  }
+
+  public saveCategory(category: Category) : Observable<Category> {
+    const headers = this.authService.createAuthHeaders();
+    return this.httpClient.post<Category>(environment.urlHost + "category/save", category, {headers : headers})
+  }
+
+  public deleteCategory(id_category : number) : Observable<boolean> {
+    const headers = this.authService.createAuthHeaders();
+    return this.httpClient.delete<boolean>(environment + "category/delete/" + id_category , {headers: headers})
+  }
 }
--- src/app/core/services/product/product.service.ts
+++ src/app/core/services/product/product.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Observable } from 'rxjs';
+import { Product } from '../../../shared/models/product.model';
+import { environment } from '../../../../envirorements/enviroments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(
+    private readonly httpClient : HttpClient,
+    private readonly authService : AuthService
+  ) { }
+
+  public getAllProducts() : Observable<Product[]> {
+    const headers = this.authService.createAuthHeaders();
+    return this.httpClient.get<Product[]>(environment.urlHost + "product", {headers: headers});
+  }
+
+  public saveProduct(product: Product) : Observable<Product> {
+    const headers = this.authService.createAuthHeaders();
+    return this.httpClient.post<Product>(environment.urlHost + "producto/save", product, {headers: headers});
+  }
+
+  public deleteProduct(id_product: number) : Observable<boolean> {
+    const headers = this.authService.createAuthHeaders();
+    return this.httpClient.delete<boolean>(environment.urlHost + "category/delete/" + id_product, {headers: headers});
+  }
+
 }
